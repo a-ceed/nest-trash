61bf75f7a11bdaaa8bbd05f3550c91d18e329603
Aceed <forjetx@gmail.com>
2024-05-30 17:27:47 +0300

    new reality

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import {VotesModule} from "./vote/votes.module";
 const DB_HOST = process.env.DB_HOST || 'mongodb'
 
 //Для локального разворачивания
-// const DB_HOST = process.env.DB_HOST || '127.0.0.1'
+//const DB_HOST = process.env.DB_HOST || '127.0.0.1'
 
 const DB_PORT = process.env.DB_PORT || '27017'
 const DB_URL = 'mongodb://' + DB_HOST + ':' + DB_PORT + '/trash'
